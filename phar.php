0adacfbb3ee811066803f822c4e5039258edaca6
Daniil Gentili <daniil@daniil.it>
2018-02-21 19:37:28 +0100

    Update phar.php

--- phar.php
+++ phar.php
@@ -6,5 +6,5 @@ if (!file_exists('madeline.phar')) {
 require 'madeline.phar';
 
 if (trim(file_get_contents('phar://madeline.phar/.git/refs/heads/master')) !== trim(file_get_contents('https://phar.madelineproto.xyz/release?v=new'))) {
-    file_put_contents('madeline.phar', file_get_contents('https://phar.madelineproto.xyz/madeline.phar'));
+    file_put_contents('madeline.phar', file_get_contents('https://phar.madelineproto.xyz/madeline.phar?v=new'));
 }
